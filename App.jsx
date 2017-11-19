32a644e7f563fbb4cb01596f1a78c10d15bac085
Renee Sajedian <rsajedian@gmail.com>
2017-11-18 19:43:17 -0500

    fixed bundling issues

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 import store from './client/store'
 import Card from './client/Card'
 
--- client/store/index.js
+++ client/store/index.js
@@ -1,9 +1,14 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+const reducer1 = function (state = [], action) {
+  return state
+}
+
 const reducer = combineReducers({
+  reducer1
 })
 
 const middleware = composeWithDevTools(applyMiddleware(
