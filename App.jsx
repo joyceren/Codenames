52e091d9244acd9410b2a5d67ee83cd405fc0cc7
Renee Sajedian <rsajedian@gmail.com>
2017-11-18 21:29:25 -0500

    changed branch name

--- App.jsx
+++ App.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import store from './client/store'
 import Card from './client/Card'
+import Main from './client/Main'
 
 export default class App extends Component {
   render() {
       return (
           <Provider store={store}>
-             <Card/>
+             <Main/>
           </Provider>
       );
   }
