a8c2f85b2c3d141ebea6646ea710070b75ef6ff2
Joyce Ren <joyce.y.ren@gmail.com>
2017-11-19 21:49:00 -0500

    frontend colors change on click (now with reducers)

--- client/Main.jsx
+++ client/Main.jsx
@@ -1,71 +1,39 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
 import Card from './Card.jsx'
 import stack from '../public/stack.png'
 
-export default class Main extends Component {
-
-  constructor() {
-    super()
-    this.state={
-      testArr:[
-        {word: "butterfly", color: "blue", flipped: false},
-        {word: "lamp", color: "blue", flipped: false},
-        {word: "Germany", color: "blue", flipped: false},
-        {word: "lightning", color: "blue", flipped: false},
-        {word: "ladder", color: "blue", flipped: false},
-        {word: "blanket", color: "blue", flipped: false},
-        {word: "flag", color: "blue", flipped: false},
-        {word: "grass", color: "blue", flipped: false},
-
-        {word: "needle", color: "red", flipped: false},
-        {word: "clock", color: "red", flipped: false},
-        {word: "basket", color: "red", flipped: false},
-        {word: "medicine", color: "red", flipped: false},
-        {word: "bottle", color: "red", flipped: false},
-        {word: "cabinet", color: "red", flipped: false},
-        {word: "plane", color: "red", flipped: false},
-        {word: "brick", color: "blue", flipped: false},
-
-        {word: "television", color: "white", flipped: false},
-        {word: "table", color: "white", flipped: false},
-        {word: "squirrel", color: "white", flipped: false},
-        {word: "xylophone", color: "white", flipped: false},
-        {word: "orchid", color: "white", flipped: false},
-        {word: "opal", color: "white", flipped: false},
-        {word: "chair", color: "white", flipped: false},
-        {word: "pillow", color: "white", flipped: false},
-        {word: "Indiana", color: "black", flipped: false}
-      ]
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick(e) {
-    e.preventDefault()
-    this.setState({testArr: this.state.testArr.map(word => {
-      return word.word===e.target.id ? {word:word.word, color:word.color, flipped: true} : word
-    })})
-  }
-
-  render() {
-    const {testArr}=this.state
-    return(
-      <div>
-        <nav>
-          <img src={stack} className="hamburger"/>
-          <h1>CODENAMES</h1>
-          <div></div>
-        </nav>
-        <div className="board">
-        {
-          testArr.map(word => {
-            return (
-              <Card handleClick={this.handleClick} key={word.word} word={word} />
-            )
-          })
-        }
-        </div>
+function Main (props) {
+  const {cards, handleClick, changeView }=props
+  return(
+    <div>
+      <nav>
+        <img src={stack} className="hamburger" />
+        <h1>CODENAMES</h1>
+        <div onClick={changeView}><h6>Change View</h6></div>
+      </nav>
+      <div className="board">
+      {
+        cards.map(word => {
+          return (
+            <Card key={word.word} word={word} />
+          )
+        })
+      }
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+const mapState = state => ({
+  cards: state.cards,
+})
+
+const mapDispatch = dispatch => ({
+  changeView(e){
+    e.preventDefault()
+    dispatch({type:"CHANGE_VIEW"})
+  }
+})
+
+export default connect(mapState, mapDispatch)(Main)
--- client/store/index.js
+++ client/store/index.js
@@ -3,17 +3,59 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const card = function (state = {}, action) {
-    switch(action.type){
+const testArr = [
+  {word: "butterfly", color: "blue", flipped: false},
+  {word: "lamp", color: "blue", flipped: false},
+  {word: "Germany", color: "blue", flipped: false},
+  {word: "lightning", color: "blue", flipped: false},
+  {word: "ladder", color: "blue", flipped: false},
+  {word: "blanket", color: "blue", flipped: false},
+  {word: "flag", color: "blue", flipped: false},
+  {word: "grass", color: "blue", flipped: false},
+
+  {word: "needle", color: "red", flipped: false},
+  {word: "clock", color: "red", flipped: false},
+  {word: "basket", color: "red", flipped: false},
+  {word: "medicine", color: "red", flipped: false},
+  {word: "bottle", color: "red", flipped: false},
+  {word: "cabinet", color: "red", flipped: false},
+  {word: "plane", color: "red", flipped: false},
+  {word: "brick", color: "blue", flipped: false},
+
+  {word: "television", color: "white", flipped: false},
+  {word: "table", color: "white", flipped: false},
+  {word: "squirrel", color: "white", flipped: false},
+  {word: "xylophone", color: "white", flipped: false},
+  {word: "orchid", color: "white", flipped: false},
+  {word: "opal", color: "white", flipped: false},
+  {word: "chair", color: "white", flipped: false},
+  {word: "pillow", color: "white", flipped: false},
+  {word: "Indiana", color: "black", flipped: false}
+]
+
+const cards = function (state = testArr, action) {
+  switch(action.type){
     case "FLIP_CARD":
-      return action.card
+      return state.map( word =>
+        word.word===action.card ? {word:word.word, color:word.color, flipped: true} : word
+      )
+    default:
+      return state
+  }
+}
+
+const spyMaster = function (state = false, action) {
+  switch(action.type){
+    case "CHANGE_VIEW":
+      return !state
     default:
       return state
   }
 }
 
 const reducer = combineReducers({
-  card
+  cards,
+  spyMaster,
 })
 
 const middleware = composeWithDevTools(applyMiddleware(
